2d4843c04f7c2a120da6cc6f832269fefff4476e
Jordan Muncrief <jordean89@gmail.com>
2020-08-18 15:14:39 -0600

    Added countries.js countries object

--- script.js
+++ script.js
@@ -1,28 +1,40 @@
 
+function getNews(){
 
-// queryURL = "https://api.currentsapi.services/v1/latest-news?language=us&apiKey="
-// apiKey = "QV6iMyFujpD8AxaFhTYC2WEehBRq35sZksMjudCZplB6umpn"
+        
+    
+    
+    let queryURL = "https://newscatcher.p.rapidapi.com/v1/search?media=True&lang=en&q=albuquerque";
+    
+    // Object passes paramaters into AJAX call to Newscatcher
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": queryURL,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "newscatcher.p.rapidapi.com",
+            "x-rapidapi-key": "e8b4a0ee00msh67c0789ad94f323p1e149djsn74ac17606489"
+        }
+    }
+    $.ajax(settings).done(function (response) {
+        console.log("News: ", response);
+    });
 
-// console.log(queryURL + apiKey);
-
-// $.ajax({
-//     url: queryURL + apiKey,
-//     method: "GET"
-// }).then(function(response){
-//     console.log(response);
-// })
-
-var settings = {
-	"async": true,
-	"crossDomain": true,
-	"url": "https://newscatcher.p.rapidapi.com/v1/search_free?media=True&lang=en&q=toyota",
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "newscatcher.p.rapidapi.com",
-		"x-rapidapi-key": "e8b4a0ee00msh67c0789ad94f323p1e149djsn74ac17606489"
-	}
 }
 
-$.ajax(settings).done(function (response) {
-	console.log(response);
-});
+getNews();
+
+function getWeather(){
+
+    
+    let queryURL = "https://cors-anywhere.herokuapp.com/api.openweathermap.org/data/2.5/weather?q=phoenix,AZ,US&appid=a646f545e8cb9250179e0c2cd1a2515f"
+    
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log("Weather: ", response)
+    })
+
+}
