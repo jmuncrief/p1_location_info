86f2a410b39cabd22b8db14649f8262ec627507e
rachelsakles <68452205+rachelsakles@users.noreply.github.com>
2020-08-19 14:38:48 -0700

    rest api (#5)

--- script.js
+++ script.js
@@ -133,46 +133,6 @@ function getRecd() {
     })
 }
 
-// getWeather();
-// getNews();
-// getRecd();
-
-// when user searches city name
-
-// return card with city, headlines, crime data, weather
-
-// hide cards till search 
-
-// let searchButton = document.getElementById('search-btn')
-
-// function getNews(){
-
-
-    
-    
-//     let queryURL = "https://newscatcher.p.rapidapi.com/v1/search?media=True&lang=en&q=albuquerque";
-    
-//     // Object passes paramaters into AJAX call to Newscatcher
-//     let settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": queryURL,
-//         "method": "GET",
-//         "headers": {
-    //             "x-rapidapi-host": "newscatcher.p.rapidapi.com",
-    //             "x-rapidapi-key": "e8b4a0ee00msh67c0789ad94f323p1e149djsn74ac17606489"
-    //         }
-    //     }
-    //     $.ajax(settings).done(function (response) {
-        //         const articles = response.articles;
-        
-        //         console.log("News: ", articles);
-        //     });
-        
-        // }
-        
-        $('.dropdown-trigger').dropdown(); 
-
 function getWeather(){
 
     // Hard-coded variables stand-in for JQuery selectors to retrieve values from search on page
@@ -216,13 +176,35 @@ function getWeather(){
         const humidity = response.main.humidity;
 
         // console.log("Weather: ", response)
-        console.log("Weather: ", "City: " + city + " Country: " + country + " Wind Speed: " + windSpeed + " Wind Direction: " + windDeg + " Current Temperature: " + tempCurrent + " High Temperature: " + tempMaxF + " Low Temperature: " + tempMinF + " Feels Like: " + heatIndex + " Humidity: " + humidity);
-        
+        console.log("Weather: ", "City: " + city + " Country: " + country + " Wind Speed: " + windSpeed + " Wind Direction: " + windDeg + " Current Temperature: " + tempCurrent + " High Temperature: " + tempMax + " Low Temperature: " + tempMin + " Feels Like: " + heatIndex + " Humidity: " + humidity);
     })
-
     
 }
 
+getNews();
+getWeather(); 
+
+
+function getRestaurant(){ 
+    const apiKey = "c856e1da1a9c81fb77e457de7e16c942";
+    const locationURL = "https://developers.zomato.com/api/v2.1/locations?query=phoenix"; 
+    //const locationDetailURL = "https://developers.zomato.com/api/v2.1/location_details?entity_id=301&entity_type=city";  
+
+    $.ajax({ 
+        url: locationURL, 
+        method: "GET" 
+    }) 
+    .then(function(response){ 
+        console.log(locationURL);
+
+
+
+    })
+
+
+
+
+}
 
 // getNews();
 getWeather();
