8a7f8b84ce68ccea809fcdf38619fd76e46b5e41
Jordan Muncrief <jordean89@gmail.com>
2020-08-21 11:55:15 -0600

    Re-writing functions

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const input = $("#textarea2");
 const searchBtn = $("#search");
 const stateBtn = $("#dropdown1");
 
-$(document).ready(function() {
+$(document).ready(function () {
     populateDropdown();
     $('select').formSelect();
 })
@@ -16,19 +16,21 @@ function populateDropdown() {
         x.attr("value", states[prop]);
         stateBtn.append(x);
     }
-    
+
 }
 
 function startSearch() {
     let txt = input.val();
     let state = stateBtn.val();
 
+
     getWeather(txt, state);
     // getRecd(state);
-    getNews(txt);
+    // getNews(txt);
+
+
 
 }
-
 
 function getInitialsByState(state) {
     for (var initials in states) {
@@ -37,14 +39,8 @@ function getInitialsByState(state) {
     }
 }
 
-// for (var inits in states) {
-//     console.log(inits);
-// }
-
 function getNews(city) {
     // API Documentation - https://www.notion.so/API-Documentation-e15cc61b6c1c4b0a904392f034779653
-
-
 
     let queryURL = "https://newscatcher.p.rapidapi.com/v1/search?media=True&lang=en&q=" + city;
 
@@ -67,13 +63,12 @@ function getNews(city) {
         for (let i = 0; i < articles.length; i++) {
             let articleTitle = articles[i].title;
             let articleLink = articles[i].link;
-            console.log(articleTitle);
-            console.log(articleLink);
+            // console.log(articleTitle);
+            // console.log(articleLink);
         }
     });
 
 }
-
 
 function getWeather(cityIn, state) {
     // API Documentation - https://openweathermap.org/current
@@ -100,29 +95,41 @@ function getWeather(cityIn, state) {
     const corsBypassProxy = "https://cors-anywhere.herokuapp.com/";
     const queryURL = corsBypassProxy + "api.openweathermap.org/data/2.5/weather?q=" + searchTerm + "&appid=" + apiKey;
 
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    }).then(function (response) {
-        const city = response.name;
-        const country = response.sys.country;
-        const windSpeed = response.wind.speed;
-        const windDeg = response.wind.deg;
-        const tempCurrentK = response.main.temp;
-        const tempMinK = response.main.temp_min;
-        const tempMaxK = response.main.temp_max;
-        const tempCurrentC = (parseInt(tempCurrentK)) - 273.15;
-        const tempMinC = (parseInt(response.main.temp_min)) - 273.15;
-        const tempMaxC = (parseInt(response.main.temp_max)) - 273.15;
-        const tempCurrentF = (tempCurrentC * 1.8) + 32;
-        const tempMinF = (tempMinC * 1.8) + 32;
-        const tempMaxF = (tempMaxC * 1.8) + 32;
-        const heatIndex = response.main.feels_like;
-        const humidity = response.main.humidity;
+    return (
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        })
+    );
+    // // .then(function (response) {
+    //     const city = response.name;
+    //     const country = response.sys.country;
+    //     const windSpeed = response.wind.speed;
+    //     const windDeg = response.wind.deg;
+    //     const tempCurrentK = response.main.temp;
+    //     const tempMinK = response.main.temp_min;
+    //     const tempMaxK = response.main.temp_max;
+    //     const tempCurrentC = (parseInt(tempCurrentK)) - 273.15;
+    //     const tempMinC = parseInt(tempMinK) - 273.15;
+    //     const tempMaxC = parseInt(tempMaxK) - 273.15;
+    //     const tempCurrentF = ((tempCurrentC * 1.8) + 32).toFixed(0);
+    //     const tempMinF = ((tempMinC * 1.8) + 32).toFixed(0);
+    //     const tempMaxF = ((tempMaxC * 1.8) + 32).toFixed(0);
+    //     const heatIndex = response.main.feels_like;
+    //     const humidity = response.main.humidity;
 
-        // console.log("Weather: ", response)
-        console.log("Weather: ", "City: " + city + " Country: " + country + " Wind Speed: " + windSpeed + " Wind Direction: " + windDeg + " Current Temperature: " + tempCurrent + " High Temperature: " + tempMaxF + " Low Temperature: " + tempMinF + " Feels Like: " + heatIndex + " Humidity: " + humidity);
-    })
+    //     // console.log("Weather: ", response)
+    //     // console.log("Weather: ", "City: " + city + " Country: " + country + " Wind Speed: " + windSpeed + " Wind Direction: " + windDeg + " Current Temperature: " + tempCurrentF + " High Temperature: " + tempMaxF + " Low Temperature: " + tempMinF + " Feels Like: " + heatIndex + " Humidity: " + humidity);
+
+    //     let weatherObj = {
+    //         "temperature":tempCurrentF,
+    //         "windSpeed":windSpeed,
+    //         "humidity":humidity
+    //     }
+
+    //     return(weatherObj);
+
+    // })
 
 }
 
@@ -160,77 +167,35 @@ function getRecd(searchTerm) {
                 let recAreaPhone = response.RecAreaPhone;
 
                 // console.log(response);
-                console.log("Rec Area Name:", recAreaName);
-                console.log("Description:", recAreaDescription);
-                console.log("Phone:", recAreaPhone);
+                // console.log("Rec Area Name:", recAreaName);
+                // console.log("Description:", recAreaDescription);
+                // console.log("Phone:", recAreaPhone);
             })
         }
     })
 }
 
 
-function getRestaurant() { 
+function getRestaurant() {
     //API documentation - https://developers.zomato.com/documentation#!/restaurant/restaurant_0 
-    const apiKey = "c856e1da1a9c81fb77e457de7e16c942"; 
+    const apiKey = "c856e1da1a9c81fb77e457de7e16c942";
     const corsBypassProxy = "https://cors-anywhere.herokuapp.com/";
     const locationURL = "https://developers.zomato.com/api/v2.1/cities?q=phoenix";
-    const locationDetailURL = "https://developers.zomato.com/api/v2.1/location_details?entity_id=301&entity_type=city";  
+    const locationDetailURL = "https://developers.zomato.com/api/v2.1/location_details?entity_id=301&entity_type=city";
 
     $.ajax({
         url: corsBypassProxy + locationURL + apiKey,
         method: "GET"
     })
         .then(function (response) {
-            console.log(locationURL);
-
-
+            // console.log(locationURL);
 
         })
 
 
 }
 
-
-// when user searches city name
-
-// return card with city, headlines, crime data, weather
-
-// hide cards till search 
-
-// let searchButton = document.getElementById('search-btn')
-
-
-//     // Object passes paramaters into AJAX call to Newscatcher
-//     let settings = {
-//         "async": true,
-//         "crossDomain": true,
-//         "url": queryURL,
-//         "method": "GET",
-//         "headers": {
-    //             "x-rapidapi-host": "newscatcher.p.rapidapi.com",
-    //             "x-rapidapi-key": "e8b4a0ee00msh67c0789ad94f323p1e149djsn74ac17606489"
-    //         }
-    //     }
-    //     $.ajax(settings).done(function (response) {
-        //         const articles = response.articles;
-        
-        //         console.log("News: ", articles);
-        //     });
-        
-        // }
-
-
-//   document.addEventListener('DOMContentLoaded', function() {
-//     var elems = document.querySelectorAll('.dropdown-trigger');
-//     var instances = M.Dropdown.init(elems, options);
-//   });
-
-// Or with jQuery
-
-  
-
-
-  searchBtn.on("click", function(e){
-      e.preventDefault();
-      startSearch();
-  })
+searchBtn.on("click", function (e) {
+    e.preventDefault();
+    startSearch();
+})
